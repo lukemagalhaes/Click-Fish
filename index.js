998103a57d2b5089f60783e7f943962c123b1d79
lukemagalhaes <lukemmagalhaes@gmail.com>
2022-12-17 17:53:50 -0300

    new file:   img/golden-fish.png 	new file:   img/wood-9000.png 	new file:   img/wood-background.jpg 	new file:   img/wood.png 	modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,108 +1,131 @@
-const canvas = document.getElementById('canvas1');
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const upgrade = document.getElementById("upgrade");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var clicks = 0
-let bonusClick = 1
-let cleanTop = false
-let cleanBottom = false
-let cleanRight = false
-let cleanLeft = false
+let clicks = 0;
+let bonusClick = 1;
+let fishUpgrade = 1;
+let num = 0;
 
+let cleanTop = false;
+let cleanBottom = false;
+let cleanRight = false;
+let cleanLeft = false;
 
 const entities = [];
-//console.log(entities)
-
-
-const fishLeft1 = new Image();
-fishLeft1.src = "img/fish1l.png";
-const fishRight1 = new Image();
-fishRight1.src = "img/fish1r.png";
-
-    
+const imgFish = [
+    {
+        fishLeft: "img/fishLeft1.png",
+        fishRight: "img/fishRight1.png",
+        width: 73,
+        height: 40,
+        bonusClick: 1,
+        price: 0
+    },
+    {
+        fishLeft: "img/fish1l.png",
+        fishRight: "img/fish1r.png",
+        width: 88,
+        height: 30,
+        bonusClick: 2,
+        price: 10
+    },
+    {
+        fishLeft: "img/fish1l.png",
+        fishRight: "img/fish1r.png",
+        width: 88,
+        height: 30,
+        bonusClick: 3,
+        price: 1000
+    }
+];
+let price = imgFish[fishUpgrade].price;
+document.getElementById("price").innerHTML = price;
 
 canvas.addEventListener('click', function (e) {
-    
-    this.x = e.x
-    this.y = e.y
-    
-    
-    let dWidth = 88;
-    let dHeight = 30;
-    
-    entities.push(new Fish(this.img, this.x, this.y, dWidth, dHeight));
-    if (bonusClick > 1) {
-        entities.push(new Fish(this.img, this.x, this.y, dWidth, dHeight));
-        clicks = clicks += 1;
-    }
-    //console.log(this.x, this.y);
+    num = Math.floor(Math.random() * fishUpgrade);
+    this.x = e.pageX;
+    this.y = e.pageY;
+    this.dWidth = imgFish[num].width;
+    this.dHeight = imgFish[num].height;
+    this.img = imgFish[num];
 
-    clicks = clicks += 1;
-    //console.log(clicks);
+
+    
+    document.getElementById("price").innerHTML = price;
+
+    entities.push(new Fish(this.img, this.x, this.y, this.dWidth, this.dHeight));
+    clicks = clicks + imgFish[num].bonusClick;
+
     document.getElementById("clicks").innerHTML = clicks;
     
     if (entities.length >= 30) {
-
-        cleanBottom = true
-        cleanTop = true
-        cleanLeft = true
-        cleanRight = true
-        //setTimeout (function() { numberFish = 0}, 2000)
-    }
-
+        cleanBottom = true;
+        cleanTop = true;
+        cleanLeft = true;
+        cleanRight = true;
+    } 
     else {
-        cleanTop = false
-        cleanBottom = false
-        cleanLeft = false
-        cleanRight = false
-        
+        cleanTop = false;
+        cleanBottom = false;
+        cleanLeft = false;
+        cleanRight = false;
     }
 
     for (let i = 0; i < entities.length; i++){
-        //console.log(entities.length)
-        if (entities[i].y - dHeight > canvas.height || entities[i].y < -dHeight || entities[i].x > canvas.width || entities[i].x < -dWidth){
-                entities.splice(i, 1);
-                //console.log(entities[i].x, entities[i].y)
+        if (entities[i].y - this.dHeight > canvas.height || entities[i].y < - this.dHeight || entities[i].x > canvas.width || entities[i].x < - this.dWidth){
+            console.log(entities);
+            entities.splice(i, 1);
         }
     }
-    console.log(entities.length, entities)
-
 });
 
 
+
+upgrade.onclick = (() => {
+    if (clicks >= price) {
+        clicks -= price;
+        fishUpgrade++;
+        document.getElementById("clicks").innerHTML = clicks;
+        price = imgFish[fishUpgrade].price;
+        document.getElementById("price").innerHTML = price;
+    }
+});
+
+function clickX2(){
+    bonusClick = bonusClick * 2;
+}
+
 class Fish {
+    src = "";
     constructor(img, x, y, dWidth, dHeight){
         this.x = x;
         this.y = y;
         this.dWidth = dWidth;
         this.dHeight = dHeight;
-        this.img = img;
+        this.src = img;
+        this.img = new Image(img.width, img.height);
         
         this.dx = Math.floor(Math.random() * 4) + 2;
-        this.dx *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
         this.dy = Math.floor(Math.random() * 4) + 2;
+        this.dx *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
         this.dy *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+
+        (this.dx < 0 ? this.img.src = this.src.fishLeft : this.img.src = this.src.fishRight)
+        
+
     }
     
     draw(){
-        if (this.dx < 0) {
-            this.img = fishLeft1;
-        }
-        else {
-            this.img = fishRight1;
-        }
         ctx.drawImage(this.img, this.x, this.y, this.dWidth, this.dHeight);
-
-        
     }
 
     animate(){
-
         this.x += this.dx;
         this.y += this.dy;
-        console.log(this.dx, this.x)
-        //console.log(this.dy, this.y)
+
         if (!cleanBottom) {
             if (this.y + this.dHeight >= canvas.height) {
                 if (this.dy > 0) {
@@ -136,6 +159,7 @@ class Fish {
                     
                 }
                 else {
+                    this.img.src = this.src.fishLeft;
                     this.dx = -this.dx;
                 }
             }
@@ -149,32 +173,28 @@ class Fish {
 
         if (!cleanLeft) {
             if (this.x < 0 && this.dx < 0) {
+                this.img.src = this.src.fishRight;
                 this.dx = -this.dx;
+                
             }
         }
         else {
             cleanLeft = true;
         }
-
+        
         this.draw()
     }
 }
 
 function Update () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
     for (let i = 0; i < entities.length; i++) {
-     let entitie = entities[i];
-     entitie.animate();
+        let entitie = entities[i];
+        entitie.animate();
     }
-    
     requestAnimationFrame(Update);
 }
 Update();
-
-
-
-
 
 const menu = document.getElementById('menu');
 let menuOpen = false;
@@ -183,6 +203,7 @@ menu.addEventListener('click', () => {
     if(!menuOpen) {
         menu.classList.add('open');
         menuOpen = true;
+        console.log(entities);
     } 
     else {
         menu.classList.remove('open');
@@ -190,3 +211,4 @@ menu.addEventListener('click', () => {
     }
 });
 
+const menuItems = document.querySelectorAll('#shop');
